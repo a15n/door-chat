d075d5f605918ef135a3ad3ae03c2548f9177c59
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-11 18:42:04 -0800

    begin at bottom of scrollable area

--- src/components/ChatPage/MainBody/component.js
+++ src/components/ChatPage/MainBody/component.js
@@ -6,28 +6,38 @@ class MainBody extends Component {
     super()
     this.renderMessage = this.renderMessage.bind(this);
   }
+  scrollToBottom() {
+    const scrollToElement = document.querySelector('.scrollToElement');
+    scrollToElement.scrollIntoView({behavior: "smooth"});
+  }
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
   renderMessage(message, i) {
     return (
-        <div key={i}>
-            <h2 className="MainBody-theirMessage">{message.message}</h2>
-            <h4 className="MainBody-name">{message.name}</h4>    
-        </div>
+      <div key={i}>
+        <h2 className="MainBody-theirMessage">{message.message}</h2>
+        <h4 className="MainBody-name">{message.name}</h4>
+      </div>
     )
   }
   render() {
-    // TODO make sure it begins at the bottom of the scrollable area
     const { roomMessages, username } = this.props;
     return (
       <div className="MainBody">
         <div className="MainBody-scrollableArea">
           {roomMessages.map(this.renderMessage)}
           <div className="MainBody-myMessages">
-            <div>
+            <div> {/* needed for flexbox */}
               <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
               <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
               <h4 className="MainBody-name MainBody-myName">{username}</h4>
             </div>
           </div>
+          <div className="scrollToElement"></div>
         </div>
       </div>
     )
