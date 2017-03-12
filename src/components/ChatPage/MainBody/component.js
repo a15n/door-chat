21a6a5e71c8ef108a60fd1da55dd561b4a93ae4b
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-11 18:09:10 -0800

    fix the box-shadow issues

--- src/components/ChatPage/MainBody/component.js
+++ src/components/ChatPage/MainBody/component.js
@@ -17,19 +17,20 @@ class MainBody extends Component {
   render() {
     // TODO make sure it begins at the bottom of the scrollable area
     return (
-      <div className='MainBody'>
-        {this.props.roomMessages.map(this.renderMessage)}
-        <div className="MainBody-myMessages">
-          <div>
-            <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
-            <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
-            <h4 className="MainBody-name MainBody-myName">Andrew</h4>
+      <div className="MainBody">
+        <div className="MainBody-scrollableArea">
+          {this.props.roomMessages.map(this.renderMessage)}
+          <div className="MainBody-myMessages">
+            <div>
+              <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
+              <h2 className="MainBody-myMessage">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas assumenda nobis voluptates placeat totam, et optio ullam, pariatur vero eveniet, accusamus minima amet repudiandae ipsa facere ea officia corporis veniam!</h2>
+              <h4 className="MainBody-name MainBody-myName">Andrew</h4>
+            </div>
           </div>
         </div>
       </div>
     )
   }
-
 }
 
 MainBody.propTypes = {
