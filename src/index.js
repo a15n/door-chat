7f9ba53657086a5200c9262f73bdae600fe41bbd
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-13 10:12:02 -0700

    serve LoginPage on '/' URL

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 
-import HomePage from './components/HomePage/component';
 import LoginPage from './components/LoginPage/component';
 import ChatPage from './components/ChatPage/component';
 import NotFound from './components/NotFound/component';
@@ -13,8 +12,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <div>
-        <Match exactly pattern='/' component={HomePage} />
-        <Match exactly pattern='/login' component={LoginPage}/>
+        <Match exactly pattern='/' component={LoginPage} />
         <Match exactly pattern='/chat/:currentRoomUrl' component={ChatPage} />
         <Miss component={NotFound} />
       </div>
